d24f304c94d4e4aab773fc701ad46c6a07168304
ulugbekarch <85783257+ulugbekarch@users.noreply.github.com>
2026-08-03 22:13:43 +0500

    Birinchi mashq

---
+++ kun1 03.08.2026.py
@@ -0,0 +1,14 @@
+# a = float(input("Kvadratning tomonini kiriting: "))
+# p = 4 * a
+# print(f"Kvadratning perimetri: {int(p)}")
+
+# a = float(input(" Kvadratning tomoni: "))
+# S = a * a
+# print(f"Kvadratning yuzasi: {int(S)} ")
+
+a = float(input("To'g'ri to'rtburchakning a tomoni: "))
+b = float(input("To'g'ri to'rtburchakning b tomoni: "))
+p = 2 * (a+b)
+S = a * b
+print(f"To'g'ri to'rtburchakning perimetri: {int(p)}")
+print(f"To'g'ri to'rtburchakning yuzasi: {int(S)}")
